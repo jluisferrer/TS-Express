e7190e45ceb6b32b35eca8bbc8068ab15073694c
jluisferrer <jluisferrer@gmail.com>
2024-02-23 11:06:35 +0100

    feat: dotenv library added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express"; //Importamos libreria express
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express(); // Creamos una constante con la metodo express
 
-app.listen(4000, () => {     //llamamos al metodo en el puerto 4000 
-    console.log("Server is running");
+const PORT = process.env.PORT || 4001; // 
+
+app.listen(PORT, () => {     //llamamos al metodo en el puerto 4000 
+    console.log(`Server is running on port: ${PORT}`);
 })  
