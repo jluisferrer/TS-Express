c59f12cf5d4802596040d1252e43576225b11cd3
jluisferrer <jluisferrer@gmail.com>
2024-02-23 11:18:30 +0100

    feat: healty endpoint

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,14 @@ const app = express(); // Creamos una constante con la metodo express
 
 const PORT = process.env.PORT || 4001; // 
 
+app.get('/healthy', (req , res) => {            /// Get recibe la url /healthy          
+    res.status(200).json({
+        succes:true,
+        message : "Server is healthy"
+    
+    })
+})
+
 app.listen(PORT, () => {     //llamamos al metodo en el puerto 4000 
     console.log(`Server is running on port: ${PORT}`);
 })  
