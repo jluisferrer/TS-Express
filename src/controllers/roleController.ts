615abfbbb6587a939826999901ba7c17e148731f
jluisferrer <jluisferrer@gmail.com>
2024-02-27 13:05:04 +0100

    feat:models and try catch

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { Role } from "../models/Role";
 
 export const getRoles = (req: Request, res: Response) => {
     res.status(200).json({
@@ -8,19 +9,41 @@ export const getRoles = (req: Request, res: Response) => {
     })
 }
 
-export const postRoles = (req: Request, res: Response) => {
+export const postRoles = async(req: Request, res: Response) => {
+try {
+    
 
-    const { name, email } = req.body;
 
     //recuperar la info a traves del body
 
-    console.log(name, email);
+    console.log(req.body);
+    const name = req.body.name;
+
+
+    if (name.length > 50) {
+        return res.status(400).json({
+            succes: false,
+            message: "Role name must be under 50 characters"
+      }
+        ) }
+    
+        const newRole = await Role.create({
+            name: name
+        }).save()
 
     res.status(201).json({
         succes: true,
-        message: "Role created"
+        message: "Role created",
+        data: newRole
 
     })
+} catch (error) {
+    res.status(500).json({
+    success:false,
+    message:"can't create rol",
+    error:error
+    })
+}
 }
 
 export const putRoles = (req: Request, res: Response) => {
--- src/database/db.ts
+++ src/database/db.ts
@@ -6,6 +6,8 @@ import { Users1708948518429 } from "./migrations/1708948518429-users";
 import { Authors1708949814183 } from "./migrations/1708949814183-authors";
 import { Books1708950242214 } from "./migrations/1708950242214-books";
 import { BookFavourite1708950977173 } from "./migrations/1708950977173-book_favourite";
+import { Role } from "../models/Role";
+import { User } from "../models/User";
 
 export const AppDataSource = new DataSource({
 type: "mysql",
@@ -14,7 +16,7 @@ port: Number(process.env.DB_PORT) || 3307,
 username: process.env.DB_USER || "root",
 password: process.env.DB_PASSWORD || "1234",
 database: process.env.DB_DATABASE|| "test",
-entities: [],
+entities: [Role, User],
 migrations: [Roles1708945243245, Users1708948518429, Authors1708949814183, Books1708950242214, BookFavourite1708950977173], 
 synchronize: false,
 logging: false,
